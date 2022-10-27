9d0870b339d2dcc727ca1edb257847da13461995
AndreaPanzeriDev <andpanz@libero.it>
2022-10-27 16:10:18 +0200

    comments

--- palindroma/main.js
+++ palindroma/main.js
@@ -15,17 +15,19 @@ if(word == ''){
 
 
 function Palindroma(){
-
+    //create the reverse word
     let mirrored = '';
     for(let i = word.length - 1 ; i >= 0; i--){
-        mirrored += word[i];
+        mirrored += word[i]; //add letter by letter to create the new word
     }
-    return mirrored;
+    return mirrored; //return the reverse word
 }
 
 
 if(word == Palindroma()){
+    //if the word is palindrome, say it and know it
     alert("La parola è palindroma");
 }else{
+    //if not, you're done
     alert("La parola non è palindroma");
 }
